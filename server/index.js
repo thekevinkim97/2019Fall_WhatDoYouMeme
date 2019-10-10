c0a56dfc564c411d95de788ca1509738ea96f050
Kevin Kim <BestKevin5897@client147-223.wireless.newpaltz.edu>
2019-10-10 11:42:27 -0400

    using dynamic port (or por from environment variable)

--- server/index.js
+++ server/index.js
@@ -1,27 +1,15 @@
 const express = require('express');
 const path = require('path');
 const userController = require('./controllers/Users');
+const gameController = require('./controllers/Game');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app
-    .use( function( req, res, next) {
-        // Logging
-        
-        console.log( { params: req.params, body: req.body, url: req.url, query: req.query, headers: req.headers });
-        next();
-    })
+    .get('/port', (req, res)=> res.send( "Using port: "+port))
     .use('/static', express.static( path.join( __dirname , '../NoFramework' ) ) )
-
-    .get(   '/',
-            (req, res) => res.send('Hello New Paltz!')
-        )
-    .get(   '/heb',
-        function(req, res){
-            res.send({ msg: 'Shalom World!' })
-        }
-    )
-    .use('/users', userController );
+    .use('/users', userController )
+    .use('/game', gameController );
 
 app.listen(port, () => console.log(`Running on http://localhost:${port}`));
