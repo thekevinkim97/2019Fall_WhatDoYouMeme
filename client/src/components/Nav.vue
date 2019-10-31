38d6a08e6977d78167a2da0716e4a548748721cc
Kevin Kim <BestKevin5897@client145-125.wireless.newpaltz.edu>
2019-10-31 11:48:49 -0400

    Teacher code 10/28, routing and vanbar

--- client/src/components/Nav.vue
+++ client/src/components/Nav.vue
@@ -1,43 +1,41 @@
 <template>
   <nav class="navbar" role="navigation" aria-label="main navigation">
     <div class="navbar-brand">
-        <a class="navbar-item" href="/">
-            <img alt="Vue logo" src="../assets/logo.png" style="height: 18px;">
-            W.D.Y.M.
-        </a>
+        <router-link exact-active-class="active" class="navbar-item" to="/">
+            <i class="fas fa-home fa-pull-left"></i>
+            Home
+        </router-link>
 
         <a @click.prevent="isOpen = !isOpen" role="button" class="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
-        <span aria-hidden="true"></span>
-        <span aria-hidden="true"></span>
-        <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
         </a>
     </div>
 
     <div id="navbarBasicExample" class="navbar-menu" :class="{ 'is-active' : isOpen }">
         <div class="navbar-start">
 
-        <router-link class="navbar-item" to="/">
-            <i class="fas fa-home fa-pull-left"></i>
-            Home
-        </router-link>
-        <router-link class="navbar-item" to="/about">
+        <router-link exact-active-class="active" class="navbar-item" to="/about">
             <i class="fab fa-vuejs fa-pull-left"></i>
             About
         </router-link>
-        <router-link class="navbar-item" to="/game">
+
+        <router-link exact-active-class="active" class="navbar-item" to="/game">
             <i class="fas fa-ghost fa-pull-left"></i>
             Game
         </router-link>
 
         <div class="navbar-item has-dropdown is-hoverable">
-            <a class="navbar-link">
+            <a class="navbar-link" active-class="kind-of-active">
             More
             </a>
 
             <div class="navbar-dropdown">
-            <a class="navbar-item">
-                About
-            </a>
+                <router-link class="navbar-item" to="/about" exact-active-class="active">
+                    <i class="fab fa-vuejs fa-pull-left"></i>
+                    About
+                </router-link>
             <a class="navbar-item">
                 Jobs
             </a>
@@ -79,8 +77,12 @@ export default {
 </script>
 
 <style>
-    .router-link-active {
-        background-color: red;
+    .active {
+        background-color: aliceblue;
         font-weight: bold;
+    }
+    .kind-of-active {
+        background-color: rgb(244, 248, 252);
+        font-weight: 100;
     }
 </style>
